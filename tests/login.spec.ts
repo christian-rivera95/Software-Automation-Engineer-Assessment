3a665325ced69fff7ef93e51297ea2fa304958e4
Roxi96 <roxnazapc@outlook.com>
2024-05-25 20:22:39 -0600

    Negative username test case added

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -11,3 +11,14 @@ test("Positive Login test case", async ({ page }) => {
 
   await expect(page.locator('a:has-text("Log out")')).toBeVisible();
 });
+
+test("Negative Username test case", async ({ page }) => {
+  await page.goto("https://practicetestautomation.com/practice-test-login/");
+  await page.fill("input#username", "johnDoe");
+  await page.fill("input#password", "Password123");
+  await page.getByRole("button", { name: "Submit" }).click();
+
+  await expect(page.locator("div#error")).toContainText(
+    "Your username is invalid!"
+  );
+});
