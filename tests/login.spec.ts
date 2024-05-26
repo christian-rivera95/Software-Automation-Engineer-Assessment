b2499fac3c6c07cef7135eacdc2eb793aea34e9b
Roxi96 <roxnazapc@outlook.com>
2024-05-25 20:19:16 -0600

    positive test case added

---
+++ tests/login.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+
+test("Positive Login test case", async ({ page }) => {
+  await page.goto("https://practicetestautomation.com/practice-test-login/");
+  await page.fill("input#username", "student");
+  await page.fill("input#password", "Password123");
+  await page.getByRole("button", { name: "Submit" }).click();
+
+  await expect(page.locator("h1")).toContainText("Successfully");
+  await expect(page.locator("strong")).toContainText("Congratulations");
+
+  await expect(page.locator('a:has-text("Log out")')).toBeVisible();
+});
